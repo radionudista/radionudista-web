f949885006ff859ea1caee1465fbe0b736d66ca7
Lemys Lopez <lemyskaman@gmail.com>
2025-07-29 10:47:20 -0500

    full envars set

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,18 +2,28 @@
 import React, { useState } from 'react';
 import CountdownTeaser from '../components/CountdownTeaser';
 import TwitchPlayer from '../components/TwitchPlayer';
-import { env } from '../config/env';
+import { env, isProduction } from '../config/env';
 import { logger } from '../utils/logger';
 
 const Index = () => {
   const [showTwitch, setShowTwitch] = useState(false);
   
-  const launchDate = new Date(env.LAUNCHING_DATE);
+  const getLaunchDate = () => {
+    if (!isProduction() && env.DEV_LAUNCHING_SECOUNDS > -1) {
+      const now = new Date();
+      now.setSeconds(now.getSeconds() + env.DEV_LAUNCHING_SECOUNDS);
+      return now;
+    }
+    return new Date(env.LAUNCHING_DATE);
+  };
+
+  const launchDate = getLaunchDate();
 
   logger.info('Index page loaded', {
     launchDate: launchDate.toISOString(),
     showTwitch,
-    environment: env.APP_ENVIRONMENT
+    environment: env.APP_ENVIRONMENT,
+    devSeconds: env.DEV_LAUNCHING_SECOUNDS,
   });
 
   const handleCountdownEnd = () => {
